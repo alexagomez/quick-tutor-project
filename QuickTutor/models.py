29960a6b4a689ea6f3a91d12c9deb654851c58fc
soukaryag <sg4fz@virginia.edu>
2020-02-10 15:14:49 -0500

    model init

--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -1,6 +1,18 @@
 import datetime
-
 from django.db import models
 
-# Create your models here.
-from django.db import models
+
+
+class Student(models.Model):
+    UUID = models.IntegerField(default=0)
+    firstName = models.CharField(max_length=100, default='')
+    lastName = models.CharField(max_length=100, default='')
+    email = models.CharField(max_length=100, default='')
+    rating = models.IntegerField(default=0)
+
+class Tutor(models.Model):
+    UUID = models.IntegerField(default=0)
+    firstName = models.CharField(max_length=100, default='')
+    lastName = models.CharField(max_length=100, default='')
+    email = models.CharField(max_length=100, default='')
+    rating = models.IntegerField(default=0)
