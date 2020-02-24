756f90affc8421eb8e1b5397294df36dfb111454
soukaryag <sg4fz@virginia.edu>
2020-02-24 14:54:02 -0500

    tutor tets

--- mysite/test_QuickTutor.py
+++ mysite/test_QuickTutor.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from QuickTutor.models import Student
+from QuickTutor.models import Student, Tutor
 
 class StudentTestCase(TestCase):
     def setUp(self):
@@ -8,3 +8,18 @@ class StudentTestCase(TestCase):
     def test_student_template(self):
         self.assertEqual(3, 3)
         self.assertEqual(2, 2)
+
+
+# Written By: Soukarya
+# Test Case ID: T2_1.1, T2_2.3
+class TutorModelTest(TestCase):
+    def setUp(self):
+        Tutor.objects.create(email="tu10r@virginia.edu", firstName="TutorOne")
+        Tutor.objects.create(email="bob@virginia.edu", firstName="Bob")
+
+    def test_student_model(self):
+        self.assertTrue(Tutor.objects.filter(email='tu10r@virginia.edu', firstName='TutorOne').exists())
+        self.assertTrue(Tutor.objects.filter(email='bob@virginia.edu', firstName='Bob').exists())
+        self.assertFalse(Tutor.objects.filter(email='dne@virginia.edu', firstName='NotReal').exists())
+
+
