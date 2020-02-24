b7386616c9418020f9526766facce330ae3b11ad
Brandie Young <brandieyoung98@gmail.com>
2020-02-24 14:51:28 -0500

    Student Model Test

--- mysite/test_QuickTutor.py
+++ mysite/test_QuickTutor.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
-from QuickTutor.models import Student
+from QuickTutor.models import Student, Tutor, TutorCourse
 
-class StudentTestCase(TestCase):
-    def setUp(self):
-        Student.objects.create(email="abc1ef@virginia.edu", firstName="Alex")
-
-    def test_student_template(self):
-        self.assertEqual(3, 3)
-        self.assertEqual(2, 2)
+#Brandie
+class StudentModelTest(TestCase):
+   # def setUp(self):
+        
+    def test_student_model(self):
+        obj1, created1 = Student.objects.update_or_create(email="abc123@virginia.edu", firstName="Alex", lastName="Smith", major="Art", year="First")
+        obj2, created2 = Student.objects.update_or_create(email="abc345@virginia.edu", firstName="Alex", lastName="Smith", major="Art", year="First")
+        self.assertNotEquals(obj1.email, obj2.email)
+        self.assertTrue(created1)
+        self.assertTrue(created2)
+        self.assertTrue(Student.objects.filter(email="abc123@virginia.edu").exists())
+        self.assertTrue(Student.objects.filter(email="abc345@virginia.edu").exists())
+        self.assertFalse(Student.objects.filter(email="hello@virginia.edu").exists())
+        self.assertTrue(Student.objects.filter(firstName="Alex").exists())
