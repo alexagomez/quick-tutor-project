e4b13686db13eaf3109683b8124c3c8ea2480113
Brandie Young <brandieyoung98@gmail.com>
2020-03-29 20:30:57 -0400

    many to one tutors to students on requests

--- QuickTutor/admin.py
+++ QuickTutor/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Student, Tutor, StudentRequest, RequestCourse, TutorCourse
+from .models import Student, Tutor, StudentRequest, TutorCourse
 
 admin.site.register(Student)
 admin.site.register(Tutor)
 admin.site.register(StudentRequest)
 admin.site.register(TutorCourse)
-admin.site.register(RequestCourse)
 
