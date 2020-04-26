f8fe0153c1e4cbc9cd7669ebdf283b403bacdcc2
soukaryag <sg4fz@virginia.edu>
2020-04-26 17:49:14 -0400

    stash

--- QuickTutor/models.py
+++ QuickTutor/models.py
@@ -1,11 +1,9 @@
 import datetime
 import uuid 
 from django.db import models
-from django.utils import timezone
 import random
 import os
 from datetime import datetime, date, time, timezone, timedelta
-from pytz import timezone
 
 def get_image_path(instance, filename):
     return os.path.join('photos', str(instance.username), filename)
@@ -40,8 +38,7 @@ class StudentRequest(models.Model):
     location = models.CharField(max_length=100, default='')
     meetingDetails = models.CharField(max_length=1000, default='')
     confusionMeter = models.IntegerField(default=0)  
-    requestTime = models.DateTimeField(auto_now=False, auto_now_add=False, default=timezone.now)
-
+    requestTime = models.DateTimeField(auto_now=False, auto_now_add=False, default = datetime.now())
     
     # student ID
     studentEmail = models.CharField(max_length=100, default='')
