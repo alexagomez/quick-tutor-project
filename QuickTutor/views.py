0660b1831eefcf28cfb2a9805af21185bf9f18f8
gvv8hp <gvv8hp@virginia.edu>
2020-04-26 20:19:56 -0400

    Added deleted json field to checkaccepted

--- QuickTutor/views.py
+++ QuickTutor/views.py
@@ -217,9 +217,9 @@ def make_request(request):
         location = request.POST['location']
         confusion = request.POST['confusion']
         meetingDetails = request.POST['meetingDetails']
-        dateTimeObj = datetime.now()
+        requestTime = datetime.now()
 
-        obj, created = StudentRequest.objects.update_or_create(sessionStartTime=dateTimeObj, courseName=courseName, header=header, description=description, location=location, status=0,
+        obj, created = StudentRequest.objects.update_or_create(sessionStartTime=requestTime, courseName=courseName, header=header, description=description, location=location, status=0,
         meetingDetails=meetingDetails, confusionMeter=confusion, studentEmail=currentStudent.email, studentUsername=currentStudent.email.split('@')[0])
         # RequestCourse.objects.get_or_create(request=obj,course=request.POST['subject'])
         
@@ -383,15 +383,11 @@ def checkaccepted(request):
     currentTutor = Tutor.objects.get(email=email)
     studentRequest = currentTutor.request
     studentAccepted = 0
-    if (studentRequest.tutorEmail == email):
+    if (studentRequest.tutorEmail != ''):
         studentAccepted = 1
     data = [{
-        'accepted': studentAccepted
-    }]
-    if(studentRequest.tutorEmail != '' and studentRequest.tutorEmail != email):
-        studentAccepted = 2
-    data = [{
-        'accepted': studentAccepted
+        'accepted': studentAccepted,
+        'deleted': 1 if (studentRequest == None) else 0
     }]
     return JsonResponse(data, safe=False)
 
@@ -427,9 +423,13 @@ def checkacceptedtutorcount(request):
 
 @login_required
 def checksessionstudent(request):
-    currentStudent = request.user
-    studentRequest = StudentRequest.objects.get(studentUsername=currentStudent.username)
+    currentUser = request.user
+    if (Student.objects.filter(username=currentUser.username).exists()):
+        studentRequest = StudentRequest.objects.get(studentUsername=currentUser.username)
+    else: 
+        studentRequest = StudentRequest.objects.get(tutorUsername=currentUser.username)
 
+    
     sessionEnded = studentRequest.sessionEnded
 
     startTime = studentRequest.sessionStartTime
